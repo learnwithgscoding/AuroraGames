a682c6d198ec26280f05b0bde942a4ddae2434ec
gshaffer312jfhfrebferaikhgvdkfkuysvbfsjrvub <132357591+gshaffer312@users.noreply.github.com>
2023-10-31 14:26:13 -0400

    Update script.js

--- CSGO-Clicker/js/script.js
+++ CSGO-Clicker/js/script.js
@@ -15,7 +15,7 @@ if (localStorage.getItem("imgData") === null) {
 }
 
 var currentCase = "case1";
-var acceptMoneyPerClick = 100.0;
+var acceptMoneyPerClick = 1000000.0;
 
 /*=========================Inventory============================*/
 //In inventory: weap skins
